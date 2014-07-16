d55986b99406d0ae7dc784fe02ad073337068844
Bryant Rockoff <brockoffdev@gmail.com>
2014-07-16 17:52:58 -0400

    coda2 installation set

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-# Public: Install Diffmerge.app to /Applications.
+# Public: Install Coda2.app to /Applications.
 #
 # Examples
 #
-#   include diffmerge
-class diffmerge {
-  package { 'DiffMerge':
-    provider => 'appdmg_eula',
-    source   => 'http://download-us.sourcegear.com/DiffMerge/4.2.0/DiffMerge.4.2.0.697.intel.stable.dmg'
+#   include coda2
+class coda2 {
+  package { 'Coda2':
+    provider => 'zip',
+    source   => 'http://download.panic.com/coda/Coda%202.0.14.zip'
   }
 }
