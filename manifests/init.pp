1b833441da7aa1e019c2ed00344788543205884f
Bryant Rockoff <brockoffdev@gmail.com>
2014-07-20 19:56:31 -0400

    altered init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 class coda2 {
   package { 'Coda2':
     ensure => installed,
-    provider => 'compressed_app',
+    provider => compressed_app,
     source   => 'http://download.panic.com/coda/Coda%202.0.14.zip'
   }
 }
